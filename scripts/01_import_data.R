64800db3d6d6f135a1dda5c9345fe50bffb595d1
Stijn Andeweg <57795390+Stijn-A@users.noreply.github.com>
2022-06-15 12:33:13 +0000

    Update 01_import_data.R

--- scripts/01_import_data.R
+++ scripts/01_import_data.R
@@ -5,16 +5,16 @@ source(file = "Scripts/01_Initialiseren/functies/functie_laad_data.R")
 
 # SGTF result from test
 data_teststraten_lab <-
-  functie_laad_data(loc = "Scripts/werkmappen/stijn/VE_omicron_delta/BA2/data/",
+  functie_laad_data(loc = "/PATH/data/",
                     datum = as_date("2022-04-12"))
 
 # CoronIT data 
 data_teststraten_all <-
-  functie_laad_data("Data/Teststraten/Geschoond/", as_date("2022-04-12"))
+  functie_laad_data("/PATH/data/", as_date("2022-04-12"))
 
 # kiemsurveillance data (WGS)
 data_kiemsurv_org <-
-  "/rivm/r/COVID-19/Modellering/Covid_variants/data/" %>%
+  "/PATH/data/" %>%
   list.files(full.names = T) %>%
   str_subset("Lijst_voor_Epi") %>%
   sort() %>%
